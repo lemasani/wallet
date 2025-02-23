62c19798e5ae67a42d7a7d62b550fd829fcebb70
lemasani <brianlemasan@gmail.com>
2025-02-23 17:16:52 +0300

    refact: index.ts to check if database connection is successful to launch sever

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import { errorHandler } from './middlewares/errorHandler';
 import userRoutes from './routes/userRoutes';
 import authRoutes from './routes/authRoutes';
+import  db from '../src/utils/db';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -13,25 +14,41 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Parse JSON bodies
+app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
-
 // Health check
 app.get('/', (req: Request, res: Response) => {
-	res.send('Wallet core running!');
+    res.send('Wallet core running!');
 });
 
 // Error handling middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-	errorHandler(err, req, res, next);
+    errorHandler(err, req, res, next);
 });
 
+// Connect to the database and start server
+async function startServer() {
+    try {
+        await db.$connect();
+        console.log('Successfully connected to the database');
+        
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Failed to connect to the database:', error);
+        process.exit(1);
+    }
+}
 
-// Start the server
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+startServer();
+
+// Cleanup on server shutdown
+process.on('SIGINT', async () => {
+    await db.$disconnect();
+    process.exit(0);
 });
