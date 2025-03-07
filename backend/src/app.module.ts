8d4ba6e668f032ea0ab004fd1b55294aec025285
lemasani <brianlemasan@gmail.com>
2025-03-07 19:59:39 +0300

    resolving issues of double git repos

---
+++ backend/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { APP_GUARD } from "@nestjs/core";
+import {ThrottlerGuard, ThrottlerModule} from "@nestjs/throttler";
+import { UserModule } from './user/user.module';
+import { WalletModule } from './wallet/wallet.module';
+
+@Module({
+  imports: [DatabaseModule, ThrottlerModule.forRoot([{
+    name: "short",
+    ttl: 1000,
+    limit: 3,
+  },{
+    name: "long",
+    ttl: 60000,
+    limit: 100,
+  }]), UserModule, WalletModule],
+  controllers: [AppController],
+  providers: [AppService, {provide: APP_GUARD, useClass: ThrottlerGuard  }],
+})
+export class AppModule {}
---
+++ backend/src/wallet/dto/wallet.dto.ts
@@ -0,0 +1,18 @@
+import {IsNotEmpty, IsNumber, IsString} from 'class-validator';
+import {PartialType} from '@nestjs/mapped-types';
+
+export class CreateWalletDto {
+	@IsNotEmpty()
+	@IsString()
+	name: string;
+	
+	@IsNotEmpty()
+	@IsNumber()
+	balance: number;
+	
+	@IsNotEmpty()
+	@IsString()
+	userId: string;
+}
+
+export class UpdateWalletDto extends PartialType(CreateWalletDto) {}
